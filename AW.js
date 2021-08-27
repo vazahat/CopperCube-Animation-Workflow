cc8ccfa2f87e6bb1f58659fa153cbf53c823da7c
Vazahat Pathan <vazahatk@yahoo.com>
2021-08-27 16:54:30 +0530

    test

--- AW.js
+++ AW.js
@@ -2,21 +2,32 @@ var scrX = ccbGetScreenWidth();
 var scrY = ccbGetScreenHeight();
 
 ccbSetWorkingDirectory("AW//");
-function onFrameDrawing()
+var UI = {};
+UI.Panel = { panelbuffer: [] };
+
+/*function onFrameDrawing()
 {
    // draw a red, transparent rectangle at the position of the mouse
  var mouseX = ccbGetMousePosX();
  var mouseY = ccbGetMousePosY();
  var menubar = ccbDrawColoredRectangle(color(30,30,30,255), 0, 0, scrX, 32);
  var statusbar = ccbDrawColoredRectangle(color(48,48,48,255), 0, scrY-32, scrX, scrY);
- panel_2d("[THE QUICK BROWN FOX + {JUMPS OVER} - (THE LAZY DOG) = 0123456789]",0,color(30,30,30,255),16,20,20,30,40);
- panel_2d("[the quick brown fox + {jumps over} - (the lazy dog) = 0123456789]",0,color(30,30,30,255),16,20,70,30,40);
-}
+//panel_2d("[THE QUICK BROWN FOX + {JUMPS OVER} - (THE LAZY DOG) = 0123456789]",0,color(30,30,30,255),16,20,20,30,40);
 
+}
+ccbRegisterOnFrameEvent(onFrameDrawing);*/
 // textbox overlay function // need to add a for loop for going through string and draw them on screen.
-function panel_2d(string,letter_spacing,color,Fontsize,X1,Y1,X2,Y2){
-	
-	
+
+
+//this is new untested code.
+
+panel_2d("menu",true,"[THE QUICK BROWN FOX + {JUMPS OVER} - (THE LAZY DOG) = 0123456789]",0,color(30,30,30,255),16,20,20,30,40);
+
+
+
+function panel_2d(name,bool,string,letter_spacing,color,Fontsize,X1,Y1,X2,Y2){
+
+	function name(){
 	if (X2 << X1+(string.length*Fontsize)+Fontsize){
 		X2 += (Fontsize*string.length);
 	}
@@ -32,10 +43,13 @@ function panel_2d(string,letter_spacing,color,Fontsize,X1,Y1,X2,Y2){
 	ccbDrawTextureRectangleWithAlpha("font//"+strCase+string.charAt(i)+".png", X1+(i*Fontsize)+letter_spacing, Y1, X1+(i*Fontsize)+Fontsize, Y1+Fontsize+(Fontsize/2));
 	
 	}
+	}
+	if (bool == true){ccbRegisterOnFrameEvent(eval(name));}
+	else {ccbUnregisterOnFrameEvent(name);}
 	return;
 	
 }
-ccbRegisterOnFrameEvent(onFrameDrawing);
+
 
 function isLowerCase(str)
 {
