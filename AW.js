ce01e8870dbb6018d79b773ffd85d33dda30faab
Vazahat Pathan <vazahatk@yahoo.com>
2021-08-31 17:26:51 +0530

    updated hover code

--- AW.js
+++ AW.js
@@ -1,4 +1,6 @@
 ccbSetWorkingDirectory("AW//");
+var scrX = ccbGetScreenWidth();
+var scrY = ccbGetScreenHeight();
 var element2dBuffer = [];
 e2dFrameEvent = function(){
 	for(var i = 0; i < element2dBuffer.length; i++){
@@ -7,8 +9,8 @@ e2dFrameEvent = function(){
 	}
 }
 
-var primaryBtnCol = color(30,30,30,255);
-var secondaryBtnCol = color(86,128,194,255)
+var primaryBtnCol = color(30,30,30,0);
+var secondaryBtnCol = color(86,128,194,255);
 
 
 element2d = function(text, textSpacing, panelColor, onHoverColor, fontSize, X1, Y1, X2, Y2){
@@ -17,6 +19,7 @@ element2d = function(text, textSpacing, panelColor, onHoverColor, fontSize, X1, 
 	this.fontSize = fontSize;
 	this.panelColor = panelColor;
 	this.onHoverColor = onHoverColor;
+	this.NormalColor = panelColor;
 	this.X1 = X1;
 	this.X2 = X2;
 	this.Y1 = Y1;
@@ -30,12 +33,12 @@ element2d.prototype.draw = function(){
 	var X2 = this.X2;
 	var Y1 = this.Y1;
 	var Y2 = this.Y2;
-	
+	color = this.NormalColor;
 
 	if (X2 < X1+(this.text.length*this.fontSize)+this.fontSize){
 		X2 += (this.fontSize*this.text.length);
 	}
-	ccbDrawColoredRectangle(this.panelColor,X1-(this.fontSize/2),Y1,X2,Y1+Y2);
+	ccbDrawColoredRectangle(color,X1-(this.fontSize/2),Y1,X2,Y1+Y2);
 	Y1 = Y1+(this.fontSize/2);
 	//draw texture according to the string//
 	
@@ -60,17 +63,26 @@ element2d.prototype.isMouseOver = function(){
 	
 	var mouseX = ccbGetMousePosX();
 	var mouseY = ccbGetMousePosY();
+	
+
 	if(mouseX > (this.X1-(this.fontSize/2)) && mouseX < this.X2+(this.fontSize*this.text.length) && mouseY > this.Y1 && mouseY < this.Y1+this.Y2)
-	 {  /*here goes the command for hover*/ 
-		this.panelColor = secondaryBtnCol;
+	 {  
+	  this.NormalColor = this.onHoverColor;
+	  var hover = true;
 	 }
-	 else{this.panelColor = primaryBtnCol;}
+	 else{this.NormalColor = this.panelColor; hover = false}
+
+	 return hover;
 
 }
 
 
-// START FUNCTION:
-var child = new element2d("File", 0,primaryBtnCol,secondaryBtnCol,10,20,0,30,32);
+// START FUNCTION: Drawing elements//
+var Menubar = new element2d("", 0,color(30,30,30,255),color(30,30,30,255),10,0,0,scrX,32);
+var Statusbar = new element2d("", 0,color(48,48,48,255),color(30,30,30,255),10,0,scrY-32,scrX,scrY);
+var FileMenuLabel = new element2d("File", 0,primaryBtnCol,secondaryBtnCol,10,20,0,30,32);
+var FileMenuPanel = new element2d("", 0,color(30,30,30,150),color(30,30,30,150),10,20,33,300,500);
+
 // child.draw();  // this will print the correct value
 
 
